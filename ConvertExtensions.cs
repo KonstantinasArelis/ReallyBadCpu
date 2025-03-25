cfdd56d756050c518daef5f14f91ddda97b63ddf
jurisbogd <juris.bogdanas@gmail.com>
2025-03-25 18:43:48 +0200

    Added conversion extensions for byte arrays

--- ConvertExtensions.cs
+++ ConvertExtensions.cs
@@ -1,3 +1,5 @@
+using System.Text;
+
 namespace reallyBadCpu; 
 
 public static class ConvertExtensions {
@@ -9,4 +11,37 @@ public static class ConvertExtensions {
     public static string toStringBinary(this int i) => Convert.ToString(i, numberBase);
     public static string toStringBinary(this uint u) => Convert.ToString(u, numberBase);
     public static string toStringBinary(this byte b) => Convert.ToString(b, numberBase);
+    
+    public static byte[] toBytes(this string s) {
+        byte[] bytes = new byte[(s.Length + 7) / 8];
+        
+        for (int i = 0; i < bytes.Length; i++) {
+            bytes[i] = s.Substring(i * 8, 8).toByteBinary();
+        }
+
+        return bytes;
+    }
+
+    public static string toString(this byte[] bs) {
+        StringBuilder s = new();
+        
+        foreach (byte b in bs) {
+            s.Append(b.toStringBinary().zeroExtend(8));
+        }
+
+        return s.ToString();
+    }
+    
+    //Add zeroes to the front of the string if the string is too short
+    //Useful for binary numbers: '1101' becomes '00000000000000000000000000001101'
+    public static string zeroExtend(this string s, int size) {
+        int missing = size - s.Length;
+
+        if (missing <= 0) {
+            return s;
+        }
+
+        string zeroes = new('0', missing);
+        return zeroes + s;
+    }
 }
