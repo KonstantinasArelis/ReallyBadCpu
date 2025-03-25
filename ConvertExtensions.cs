63b98641f6150959877458785de978db5c7c93be
jurisbogd <juris.bogdanas@gmail.com>
2025-03-25 16:24:29 +0200

    Improved extensibility

--- ConvertExtensions.cs
+++ ConvertExtensions.cs
@@ -1,8 +1,10 @@
 namespace reallyBadCpu; 
 
 public static class ConvertExtensions {
-    public static int toIntBinary(this string s) => Convert.ToInt32(s, 2);
-    public static uint toUIntBinary(this string s) => Convert.ToUInt32(s, 2);
-    public static string toStringBinary(this int i) => Convert.ToString(i, 2);
-    public static string toStringBinary(this uint u) => Convert.ToString(u, 2);
+    public const int numberBase = 2;
+    
+    public static int toIntBinary(this string s) => Convert.ToInt32(s, numberBase);
+    public static uint toUIntBinary(this string s) => Convert.ToUInt32(s, numberBase);
+    public static string toStringBinary(this int i) => Convert.ToString(i, numberBase);
+    public static string toStringBinary(this uint u) => Convert.ToString(u, numberBase);
 }
